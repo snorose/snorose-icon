409de0dde7686e14f2b17f7cd9d27e6c7d77f471
misung-dev <misung.dev@gmail.com>
2026-07-25 14:41:38 +0900

    feat: SVGR 기반 SVG 아이콘 컴포넌트 export 구성

---
+++ src/icons/index.ts
@@ -0,0 +1,6 @@
+export { default as IconCommentFill } from './assets/basic/ic-basic-commet.svg?react';
+export { default as IconPencilFill } from './assets/basic/ic-basic-pencil.svg?react';
+export { default as IconTrashFill } from './assets/basic/ic-basic-trash.svg?react';
+export { default as IconUserFill } from './assets/basic/ic-basic-user.svg?react';
+export { default as IconFlag } from './assets/multiColor/ic-multi-flag.svg?react';
+export { default as IconShare } from './assets/multiColor/ic-multi-share.svg?react';
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
 // 아이콘 컴포넌트의 공개 진입점입니다.
-export {};
+export * from './icons';
