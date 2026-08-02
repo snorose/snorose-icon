a48c175b49a550bd526e58e7419e671bf96a3365
misung-dev <misung.dev@gmail.com>
2026-08-02 22:48:47 +0900

    chore: basic 아이콘 파일명에서 fill과 line 접미사 정리 (#10)
    * chore: basic 채움 아이콘 파일명에 fill 접미사 적용
    
    * chore: basic 라인 아이콘 파일명에서 line 접미사 제거

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,5 +1,5 @@
-export { default as IconCommentFill } from './assets/basic/ic-basic-commet.svg?react';
-export { default as IconPencilFill } from './assets/basic/ic-basic-pencil.svg?react';
+export { default as IconCommentFill } from './assets/basic/ic-basic-commet-fill.svg?react';
+export { default as IconPencilFill } from './assets/basic/ic-basic-pencil-fill.svg?react';
 export { default as IconTrashFill } from './assets/basic/ic-basic-trash.svg?react';
 export { default as IconUserFill } from './assets/basic/ic-basic-user.svg?react';
 export { default as IconFlag } from './assets/multiColor/ic-multi-flag.svg?react';
