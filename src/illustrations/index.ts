e8f953becb1e227adcd123e2dbcb76dd27ca2249
misung-dev <misung.dev@gmail.com>
2026-08-07 23:08:21 +0900

    chore: illustrations 배럴 파일을 추가하고 최상위 인덱스에서 illustrations를 외부 export로 노출

---
+++ src/illustrations/index.ts
@@ -0,0 +1,7 @@
+export { default as IllustrationBellEmpty } from './il-bell-empty.svg?react';
+export { default as IllustrationCalendarEmpty } from './il-calendar-empty.svg?react';
+export { default as IllustrationClipboardSearch } from './il-clipboard-search.svg?react';
+export { default as IllustrationCommentEmpty } from './il-comment-empty.svg?react';
+export { default as IllustrationEmailCheck } from './il-email-check.svg?react';
+export { default as IllustrationPostEmpty } from './il-post-empty.svg?react';
+export { default as IllustrationScrapPostEmpty } from './il-scrap-post-empty.svg?react';
