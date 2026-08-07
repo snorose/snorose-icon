37d052863177eed5116e0efa552e2ccf34b57187
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-08-07 21:43:14 +0900

    fix: IconCheck export 중복 세미콜론 제거

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -36,7 +36,7 @@ export { default as IconX } from './assets/basic/ic-basic-x.svg?react';
 export { default as IconBellFill } from './assets/basic/ic-basic-bell-fill.svg?react';
 export { default as IconBell } from './assets/basic/ic-basic-bell.svg?react';
 export { default as IconCheckCircle } from './assets/basic/ic-basic-check-circle.svg?react';
-export { default as IconCheck } from './assets/basic/ic-basic-check.svg?react';;
+export { default as IconCheck } from './assets/basic/ic-basic-check.svg?react';
 export { default as IconChevronDown } from './assets/basic/ic-basic-chevron-down.svg?react';
 export { default as IconChevronLeft } from './assets/basic/ic-basic-chevron-left.svg?react';
 export { default as IconChevronRight } from './assets/basic/ic-basic-chevron-right.svg?react';
@@ -68,4 +68,3 @@ export { default as IconMultiSkull } from './assets/multi/ic-multi-skull.svg?rea
 export { default as IconMultiUserQuestion } from './assets/multi/ic-multi-user-question.svg?react';
 export { default as IconMultiVideo } from './assets/multi/ic-multi-video.svg?react';
 export { default as IconMultiWarningCone } from './assets/multi/ic-multi-warning-cone.svg?react';
-
