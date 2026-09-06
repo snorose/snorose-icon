dc9c78c44c7e2521df43bc72d5723325ab8abab0
gain-0525 <kimgain0525@sookmyung.ac.kr>
2026-09-06 16:26:39 +0900

    feat: 누락된 아이콘 추가

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -16,6 +16,7 @@ export { default as IconChevronDown } from './basic/ic-basic-chevron-down.svg';
 export { default as IconChevronLeft } from './basic/ic-basic-chevron-left.svg';
 export { default as IconChevronRight } from './basic/ic-basic-chevron-right.svg';
 export { default as IconChevronUp } from './basic/ic-basic-chevron-up.svg';
+export { default as IconChevronUpRight } from './basic/ic-basic-chevron-up-right.svg';
 export { default as IconClapperboard } from './basic/ic-basic-clapperboard.svg';
 export { default as IconCommentFill } from './basic/ic-basic-comment-fill.svg';
 export { default as IconEyeFill } from './basic/ic-basic-eye-fill.svg';
